271b41416294c0eaf9c510d28762aeec93b94254
Evelien-Lillian Dekkers <maartenwut@gmail.com>
2018-03-02 09:04:18 +0100

    uploaded files

---
+++ the_rarest.sh
@@ -0,0 +1,462 @@
+#!/bin/bash
+sleep 2s
+echo -ne "\e[97m"
+
+echo -ne "NNNNNNNNNNNNNNNNNNNN"							&& sleep .07s
+echo -ne "NNNNNNNNNNNNNNNNNNNN"							&& sleep .07s
+echo -ne "NNNNNNNNNNNNNNNNNNNN"							&& sleep .07s
+echo -e  "NNNNNNNNNNNNNNNNNNNN"							&& sleep .07s
+
+echo -ne "NNNNNNNNNNNNNNNNNNNN"							&& sleep .07s
+echo -ne "NNNNNN00*##**00NNNNN"							&& sleep .07s
+echo -ne "NNNNNNNNNNNNNNNNNNNN"							&& sleep .07s
+echo -e  "NNNNNNNNNNNNNNNNNNNN"							&& sleep .07s
+
+echo -ne "NNNNNNNNNNNNNNNNNNNN"							&& sleep .07s
+echo -ne "NN0N#94\$-:::!\$#N4*40"						&& sleep .07s
+echo -ne "NNNNNNNN*#N#4#44##04"							&& sleep .07s
+echo -e  "0NNNNNNNNNNNNNNNNNNN"							&& sleep .07s
+
+echo -ne "NNNNNNNNNNNNNNNNNNNN"							&& sleep .07s
+echo -ne "*06\",,,,,,,,,,,,,\"-6"						&& sleep .07s
+echo -ne "0#*N0N6#9!',,,,,,,\"!"						&& sleep .07s
+echo -e  "6NNNNNNNNNNNNNNNNNNN"							&& sleep .07s
+
+echo -ne "NNNNNNNNNNNNNNNNNN0+"							&& sleep .07s
+echo -ne "',,,,,,,,,,,,,,,,,,,"							&& sleep .07s
+echo -ne ",':!-'\",,,,,,,,,,,,,"						&& sleep .07s
+echo -e  ",,'4#NNNNNNNNNNNNNNN"							&& sleep .07s
+
+echo -ne "NNNNNNNNNNNNNNNN#^,,"							&& sleep .07s
+echo -ne ",,,,,,,,,,,,,,,,,,,,"							&& sleep .07s
+echo -ne ",,,,,,,,,,,,,,,,,,,,"							&& sleep .07s
+echo -e  ",,,,!8NNNNNNNNNNNNNN"							&& sleep .07s
+
+echo -ne "NNNNNNNNNNNNNNN#',,,"							&& sleep .07s
+echo -ne ",,,,,,,,,,,,,,,,,,,,"							&& sleep .07s
+echo -ne ",,,,,,,,,,,,,,,,,,,,"							&& sleep .07s
+echo -e  ",,,,,#0NNNNNNNNNNNNN"							&& sleep .07s
+
+echo -ne "NNNNNNNNNNNNN06\",,,,"						&& sleep .07s
+echo -ne ",,,,,,,,,,,,,,,,,,,,"							&& sleep .07s
+echo -ne ",,,,,,,,,,,,,,,,,,,,"							&& sleep .07s
+echo -e  ",,,,,,4#0NNNNNNNNNNN"							&& sleep .07s
+
+echo -ne "NNNNNNNNNNNNN0',,,,,"							&& sleep .07s
+echo -ne ",,,,,,,,,,,,,,,,,,,,"							&& sleep .07s
+echo -ne ",,,,,,,,,,,,,,,,,,,,"							&& sleep .07s
+echo -e  ",,,,,,'\$6*4N0NNNNNNN"						&& sleep .07s
+
+echo -ne "NNNNNNNNNNNN#\",,,,,,"						&& sleep .07s
+echo -ne ",,,,,,,,,,,,,,,,,,,,"							&& sleep .07s
+echo -ne ",,,,,,,,,,,,,,,,,,,,"							&& sleep .07s
+echo -e  ",,,,,,,,,,\"'!#NNNNNN"						&& sleep .07s
+
+echo -ne "NNNNNNNNNNN#\",,,,,,,"						&& sleep .07s
+echo -ne ",,,,,\"\"\"\"'?::-Z\$5++t"					&& sleep .07s
+echo -ne "966666666+5664444446"							&& sleep .07s
+echo -e  "6669++\$55553:'9*NNNN"						&& sleep .07s
+
+echo -ne "NNNNNNN*#96',,,,,,,,"							&& sleep .07s
+echo -ne ",,,\"\":\$+99++++9++9++"						&& sleep .07s
+echo -ne "+++96666446++6444466"							&& sleep .07s
+echo -e  "99t+999996^99+3:0NNN"							&& sleep .07s
+
+echo -ne "NNNNN04!,,,,,,,,,,,,"							&& sleep .07s
+echo -ne ",,\":\$+++++9999699999"						&& sleep .07s
+echo -ne "99+5++999966\$9699999"						&& sleep .07s
+echo -e  "+++99999966####+#NNN"							&& sleep .07s
+
+echo -ne "NNNN##\",,,,,,,,,,,,,"						&& sleep .07s
+echo -ne ",,:966666666699+9996"							&& sleep .07s
+echo -ne "9+9999+\$5+9+<\$9669++"						&& sleep .07s
+echo -e  "99+9#0NNNNNNNNNNN#0N"							&& sleep .07s
+
+echo -ne "NNN0!,,,,,,,,,,,,,,,"							&& sleep .07s
+echo -ne ",\"!99+\$\$###0N###NNNN"						&& sleep .07s
+echo -ne "NNN#+9666+5\$-\$+5++++"						&& sleep .07s
+echo -e  "4#4NNNNNNNNNNNNNN00N"							&& sleep .07s
+
+echo -ne "NN0\$,,,,,,,,,,,,,,,,"						&& sleep .07s
+echo -ne ",,:64469N0NNNZ^-#NNN"							&& sleep .07s
+echo -ne "NNNNNNN0Nt9\$39+N#0NN"						&& sleep .07s
+echo -e  "*D!6NNNNNNNNNNN04*NN"							&& sleep .07s
+
+echo -ne "N0#,,,,,,,,,,,,,,,,,"							&& sleep .07s
+echo -ne ",,\"-+96666++##*0NnNN"						&& sleep .07s
+echo -ne "NNNNNNNN*+5+\$\$9##000"						&& sleep .07s
+echo -e  "04#NNNN0m#+++t+N8NNN"							&& sleep .07s
+
+echo -ne "00\",:+++++++\"\"-+:,,,"						&& sleep .07s
+echo -ne ",:+-':!\$5@0*5++++N0N"						&& sleep .07s
+echo -ne "++++++5+9+5+66+96466"							&& sleep .07s
+echo -e  "954+9999+0NN*00NNNNN"							&& sleep .07s
+
+echo -ne "*\",,0NNNNNNN3:NN0,,,"						&& sleep .07s
+echo -ne ",0N0::DNNNNNNNN::*NN"							&& sleep .07s
+echo -ne "+96NNN*99NN040NNNX64"							&& sleep .07s
+echo -e  "+NN446NNNNNNNNNNNNNN"							&& sleep .07s
+
+echo -ne ",,,,NNN?::::::NNN,,,"							&& sleep .07s
+echo -ne ",NNN:+NN5\"\"\"!NNG\"0NN"						&& sleep .07s
+echo -ne "?#NNN!:::NN*!*NNNN3!"							&& sleep .07s
+echo -e  "4NN::NNN*9NNNNN#NNNN"							&& sleep .07s
+
+echo -ne ",,,,NNNNNNN0\"\"NNN,,,"						&& sleep .07s
+echo -ne ",NNN\"NNN\",,,,,,,,0NN"						&& sleep .07s
+echo -ne "NNNN:\",,:NN8,4NNNNN5"						&& sleep .07s
+echo -e  "XNN,\$NN\"\$N0###*0NNNN"						&& sleep .07s
+
+echo -ne ",,,,NNN8444\$,\"NNN,,,"						&& sleep .07s
+echo -ne ",NNN\"NNN\",,,\"G+?,0NN"						&& sleep .07s
+echo -ne "N6NNN+,,:NN8,4NN!8NN"							&& sleep .07s
+echo -e  "NNN,!NN5\",XNNNNNNNNN"						&& sleep .07s
+
+echo -ne ",,,,NNN\",,,,,\"#NN4++"						&& sleep .07s
+echo -ne "6NN#\"3NNNDGDNNN3,0NN"						&& sleep .07s
+echo -ne "-,:*NN#\":NN8,4NN-,9N"						&& sleep .07s
+echo -e  "NNN,\"#NN09+G#NNNNNNN"						&& sleep .07s
+
+echo -ne ",,,,*N0\",,,,,,\"8NNNN"						&& sleep .07s
+echo -ne "NN#\",,-4NNNNNm3\",4NN"						&& sleep .07s
+echo -ne "\",,\"9NN4?NN6,9NN?,,-"						&& sleep .07s
+echo -e  "NNN,,\"%NNNNNNN00NNNN"						&& sleep .07s
+
+echo -ne ",,,,\"\"\",,,,,,,,\"\"\"\"\""					&& sleep .07s
+echo -ne "\"\"\",,,,\"\"\"\"\"\"\",,,\"\"\""			&& sleep .07s
+echo -ne "\",,,\"\"\"\"\"\"\"\",\"\"\"\",,,"			&& sleep .07s
+echo -e  "\"\"\",,,,\"\"\"\"\"\"6NNNNNN"				&& sleep .07s
+
+echo -ne ",,,\"\"\"\",,,\"\"\",,,,\"\"\""				&& sleep .07s
+echo -ne "\"\"\"\",,,\"\"\"\"\"\"\"\"\",,,\""			&& sleep .07s
+echo -ne "\"\"\"\",,,\"\"\"\",\"\"\",,\"\"\""			&& sleep .07s
+echo -e  "\"\"\"\"\"\",,,\"\"\"\"\"NNNNNN"				&& sleep .07s
+
+echo -ne ",,,*NNX\",,6NN,,\"9NNN"						&& sleep .07s
+echo -ne "NNN#!,,GNNNNNNNN+\"\"0"						&& sleep .07s
+echo -ne "NNN\$,,-NNNN,+NN,,mNN"						&& sleep .07s
+echo -e  "NNNNN*\",+NNNNNNNNNNN"						&& sleep .07s
+
+echo -ne ",,,NNNN#,,NNN,?NNN#9"							&& sleep .07s
+echo -ne "+DNNN+,#NN4444NNN\"\"N"						&& sleep .07s
+echo -ne "NNNN\",NNNNN,4NN,,NNN"						&& sleep .07s
+echo -e  "44444+\"\"NNN%9@NNNNNN"						&& sleep .07s
+
+echo -ne ",,,NNNNNN\"NNN,4NN?,,"						&& sleep .07s
+echo -ne ",,\"NNN,#NN888#NNN\"\"N"						&& sleep .07s
+echo -ne "N0NN\$5NNNNN,4NN,,NNN"						&& sleep .07s
+echo -e  "NNNNN5,\"NNNNNNN*NNNN"						&& sleep .07s
+
+echo -ne ",,,NNN-NNNNNN,*NN,,,"							&& sleep .07s
+echo -ne ",,,0NN,#NNNNNNN#?,\"N"						&& sleep .07s
+echo -ne "N40NNNN0NNN,4NN,,NNN"							&& sleep .07s
+echo -e  "NNNNN+,,\"-6NNNNN0NNN"						&& sleep .07s
+
+echo -ne "0:,NNN,\"NNNNN,\$NN*\"\""						&& sleep .07s
+echo -ne "\"\"%NN0,#NN,,?NNN+\"\"N"						&& sleep .07s
+echo -ne "N4\"NNNN?NNN,4NN,,NNN"						&& sleep .07s
+echo -e  "\"\"\"\"\"\"'6NN\$NN#NNN#0N"					&& sleep .07s
+
+echo -ne "0#'NNN,,,NNNN,,+NNNN"							&& sleep .07s
+echo -ne "NNNNm,,4NN,,,,#NN+\"N"						&& sleep .07s
+echo -ne "N6\"NNNN,0NN,6NN,,NNN"						&& sleep .07s
+echo -e  "NNNNN0n8NNNNNNNN4404"							&& sleep .07s
+
+echo -ne "@0D!65,,,\"GD+,,,\"\$64"						&& sleep .07s
+echo -ne "44+?\",,:6D,,,,\"\$DG\"5"						&& sleep .07s
+echo -ne "D?,?DD?,!D+,?6D,,\$D4"						&& sleep .07s
+echo -e  "46###4N0m##@@D94#4#0"							&& sleep .07s
+
+echo -ne "NNNNN85:\",,,,,,,,,,,"						&& sleep .07s
+echo -ne ",,,,,,,,,,,,,,,,,,,,"							&& sleep .07s
+echo -ne ",,,,,,,,,,,,,,,,'!60"							&& sleep .07s
+echo -e  "N6*0000mN44N#04N4#4N"							&& sleep .07s
+
+echo -ne "NNNNNNNNN8\$?\",,,,,,,"						&& sleep .07s
+echo -ne ",,,,,,,,,,,,,,,,,,,,"							&& sleep .07s
+echo -ne ",,,,,,,,,,\"\":90N6D#*"						&& sleep .07s
+echo -e  "NN000N408699D##4X9N4"							&& sleep .07s
+
+echo -ne "NNNNNNNNNNNNN0n+-:\"\""						&& sleep .07s
+echo -ne "\"\"\"\"\"\"\"\"\"\"\",,,,,,,,,"				&& sleep .07s
+echo -ne ",,\"\"'!6##9#0040N84NN"						&& sleep .07s
+echo -e  "0NN*D6N94**#dx9N6#4N"							&& sleep .07s
+
+echo -ne "NNNNNNNNNNNNNNNNNNNN"							&& sleep .07s
+echo -ne "N00*#nD699++\$--Z!::\""						&& sleep .07s
+echo -ne "!\$6#N96D048NN#NN00m8"						&& sleep .07s
+echo -e  "D666D44D69X64#*4NNNN"							&& sleep .07s
+
+echo -ne "NNNNNNNNNNNNNNNNNNNN"							&& sleep .07s
+echo -ne "NNNNNNNNNNNNNX5^X500"							&& sleep .07s
+echo -ne "96D4##6684Nm*#60mD4*"							&& sleep .07s
+echo -e  "0*DX994#04#00NNNNNNN"							&& sleep .07s
+
+echo -ne "0NNNNNNNNNNNNNNNNNNN"							&& sleep .07s
+echo -ne "NNNNNNNNNNN4#09##0**"							&& sleep .07s
+echo -ne "*NmN0000N0#N09*00#60"							&& sleep .07s
+echo -e  "#990#00NNNNNNNNNNNNN"							&& sleep .07s
+
+echo -ne "0NNNNNNNNNNNNNNNNNNN"							&& sleep .07s
+echo -ne "NNNNNNNN##^40*X40N80"							&& sleep .07s
+echo -ne "0*4#NN0*#4666*m#D46#"							&& sleep .07s
+echo -e  "440NNNNNNNNNNNNNNNNN"
+
+sleep 2s && echo ""										&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo -ne "GET OUT GET OUT GET "
+echo -ne "OUT GET OUT GET OUT "
+echo -ne "GET OUT GET OUT GET "
+echo -e  "OUT GET OUT GET OUT "							&& sleep .07s
+
+echo ""
+
+echo -e  "RRRRRRRRRRRREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+echo -e  "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"							&& sleep .07s
+
+
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && sleep .07s
+echo "" && echo "" && echo -e  "\e[97m"
+
+clear
